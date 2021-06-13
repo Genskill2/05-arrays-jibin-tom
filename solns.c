b4dc4e0aa7fc3f7641b1efcf44fa1ef5352a46ed
jibin-tom <84715203+jibin-tom@users.noreply.github.com>
2021-06-13 16:15:55 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -42,9 +42,9 @@ float average(int a[], int s){
 int mode(int a[], int s){
     
   int max_element = max(a,s);
-  int arr[max_element];
+  int arr[max_element+1];
   
-  for(int i=0; i<max_element; i++){
+  for(int i=0; i<= max_element; i++){
     arr[i]=0;
     for(int j = 0; j < s; j++){
          if(a[j] == i){
@@ -54,7 +54,7 @@ int mode(int a[], int s){
   }
   
   int l = arr[0], pos;
-  for(int i = 1; i < max_element; i++){
+  for(int i = 1; i <= max_element; i++){
    if(arr[i]>l){
      l = arr[i];
      pos=i;

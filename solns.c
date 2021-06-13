71290f40be6d0db83da65a1e069d4909ddb0fbbc
jibin-tom <84715203+jibin-tom@users.noreply.github.com>
2021-06-13 15:25:30 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -11,3 +11,29 @@ int max(int a[], int s){
   }
   return l;
 }
+
+
+int min(int a[], int s){
+  
+  int sm = a[0];
+  for(int i = 1; i < s; i++){
+   if(a[i]<sm){
+     sm = a[i];
+   }
+  }
+  return sm;
+}
+
+float average(int a[], int s){
+  
+  float sum = 0;
+  
+  for (int i = 0; i < s ; i++) {
+     
+    sum+= a[i]; 
+  }
+  
+  float avg = sum/(float)s; 
+  
+  return avg;
+}

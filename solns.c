4434fcefff0d706674d9c5e5825925bd75c918b1
jibin-tom <84715203+jibin-tom@users.noreply.github.com>
2021-06-13 15:10:06 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,13 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+int max(int a[], int s){
+  
+  int l = a[0];
+  for(int i = 1; i < s; i++){
+   if(a[i]>l){
+     l = a[i];
+   }
+  }
+  return l;
+}
